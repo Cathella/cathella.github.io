f707619c17e639e7ccdd6c6fcf50f11e3854626d
Cathella <ncathie214@gmail.com>
2021-12-27 09:13:53 +0300

    Add project descriptions

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const projects = [
     github: "https://github.com/Cathella/BooksApp",
     live: "https://cathella.github.io/BooksApp/",
     imgsrc: "img/books.png",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti minima corporis natus nihil itaque, iste numquam labore placeat. Fugit rerum reiciendis quos porro quasi non aspernatur saepe, consequuntur alias eum.",
+    description: "Building a single page application using JavaScript classes, methods and DOM manipulation.",
   },
   {
     id: "project-2",
@@ -13,7 +13,7 @@ const projects = [
     github: "https://github.com/Cathella/to-do-list",
     live: "https://cathella.github.io/to-do-list/dist/",
     imgsrc: "img/todo.png",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti minima corporis natus nihil itaque, iste numquam labore placeat. Fugit rerum reiciendis quos porro quasi non aspernatur saepe, consequuntur alias eum.",
+    description: "In this project, I build a simple HTML list of To Do tasks. The list will be styled according to the specifications listed later in this lesson. This simple web page will be built using webpack and served by a webpack dev server.",
   },
   {
     id: "project-3",
@@ -21,7 +21,7 @@ const projects = [
     github: "https://github.com/Cathella/leaderBoard",
     live: "https://amazing-almeida-714d03.netlify.app/",
     imgsrc: "img/leaderboard.png",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti minima corporis natus nihil itaque, iste numquam labore placeat. Fugit rerum reiciendis quos porro quasi non aspernatur saepe, consequuntur alias eum.",
+    description: "In this project I create a new game using an API. I Implement the Refresh button (receiving data from the API and parsing the JSON) and the form Submit button (sending data to the API). I use the async and await JavaScript features to consume the API.",
   },
   {
     id: "project-4",
@@ -29,7 +29,7 @@ const projects = [
     github: "https://github.com/Cathella/JavaScript-Group-Capstone",
     live: "https://laughing-hodgkin-321112.netlify.app/",
     imgsrc: "img/foodmood.png",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti minima corporis natus nihil itaque, iste numquam labore placeat. Fugit rerum reiciendis quos porro quasi non aspernatur saepe, consequuntur alias eum.",
+    description: "This is my JavaScript capstone project. It is a web application based on an external API. The selected API provides data about different recipes. The webapp has two interfaces. A home page showing a list of items that you can like and A popup window with more data about an item that you can use to comment on the item.",
   },
   {
     id: "project-5",
@@ -37,7 +37,7 @@ const projects = [
     github: "https://github.com/Cathella/QuizzApp",
     live: "https://cathella.github.io/QuizzApp/dist/",
     imgsrc: "img/quizzy.png",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti minima corporis natus nihil itaque, iste numquam labore placeat. Fugit rerum reiciendis quos porro quasi non aspernatur saepe, consequuntur alias eum.",
+    description: "A Quiz app that loads questions from the open trivia db API and uses event listeners to capture the users answers to them.",
   },
   {
     id: "project-6",
@@ -45,7 +45,7 @@ const projects = [
     github: "https://github.com/Cathella/math-magicians",
     live: "https://math-magicians-nakitto.netlify.app/",
     imgsrc: "img/magicians.png",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti minima corporis natus nihil itaque, iste numquam labore placeat. Fugit rerum reiciendis quos porro quasi non aspernatur saepe, consequuntur alias eum.",
+    description: "Math magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: - make simple calculations and read a random math-related quote.",
   },
   {
     id: "project-7",
@@ -53,7 +53,7 @@ const projects = [
     github: "https://github.com/Cathella/book-store",
     live: "https://github.com/Cathella/book-store",
     imgsrc: "img/bookstore.png",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti minima corporis natus nihil itaque, iste numquam labore placeat. Fugit rerum reiciendis quos porro quasi non aspernatur saepe, consequuntur alias eum.",
+    description: "Bookstore is a books website with features to display a list of books, add a book and remove a selected book from an API",
   },
 ];
 
